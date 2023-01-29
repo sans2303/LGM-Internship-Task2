14fc8a4f37ae481ae529c415f89a01978a71c295
Sanskruti <sanskruti.salunkhe@vit.edu.in>
2023-01-30 01:19:36 +0530

    task 2

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home({ getData }) {
+  return (
+    <section className="home">
+      <div className="content">
+        <p>
+          
+          Click the button in the
+          <br />
+          navbar to get the users details.
+        </p>
+      </div>
+    </section>
+  );
+}
